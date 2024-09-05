bb68703c6587a60a36a58970270d2a70cb47d08d
chephyty <soumyahembra123@gmail.com>
2024-09-06 00:04:05 +0530

    all links are added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import Typewriter from 'typewriter-effect/dist/core';
+
 const About = () => {
+    // const words = ["नमस्ते ", "Hola", "Hello", "Bonjour", "Nǐ hǎo", "Salve", "Asalaam alaikum"];
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const words = ["नमस्ते 🙏", "Hola 👋👋", "Hello", "Bonjour", "Nǐ hǎo 🫷", "Salve", "Asalaam alaikum 🤲"];
+
+    useEffect(() => {
+        const typewriter = new Typewriter('#typewriter', {
+            loop: true, // Add this if you want the animation to loop
+            autoStart: true,
+            delay: 100,
+        });
+
+        words.forEach((word) => {
+            typewriter.typeString(word).pauseFor(1000).deleteAll().start();
+        });
+    }, [words]);
+
     return (
         <div className="about">
-            <h2>Hola 👋</h2>
+            <h2 id="typewriter"></h2>
             <h3>I'm Soumya Ranjan Hembram, a dedicated Web and App developer with a passion for creating innovative, user-friendly digital solutions. My expertise spans both front-end and back-end development.</h3>
             <p>I have hands-on experience in real-world web development, utilizing a modern tech stack to build dynamic and responsive websites. My skill set includes working with the latest frameworks, libraries, and tools, allowing me to create high-quality, scalable solutions. I am continuously expanding my knowledge to stay updated with emerging technologies and industry trends.
                 <br />
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,15 +1,15 @@
-import image from "../assets/ProfileImage.png"
-const Profile = ({ }) => {
+import image from "../assets/profile.png"
+const Profile = () => {
     return (
         <div className="profile">
             <img src={image} alt="Profile of Soumya Ranjan Hembram" className="imageProfile" />
             <h3>Soumya Ra. Hembram</h3>
             <p>Web and App Developer</p>
             <ul>
-                <li><a href="">LinkedIn</a></li>
-                <li><a href="">Github</a></li>
-                <li><a href="">Email</a></li>
-                <li><a href="">Instagram</a></li>
+                <li><a href="https://www.linkedin.com/in/soumya-ranjan-hembram-87797a22b" target="_blank" rel="noreferrer">LinkedIn</a></li>
+                <li><a href="https://github.com/CHEPHYTY" target="_blank" rel="noreferrer">Github</a></li>
+                <li><a href="mailto: soumyahembra123@gmail.com">Email</a></li>
+                <li><a href="https://www.instagram.com/sanat__soumya/">Instagram</a></li>
             </ul>
             <button>Contact Me</button>
         </div>
